dbdc236b6f9a8d830dd922634fed90d3f66790aa
Talon Durrant <talonmdurrant@gmail.com>
2020-10-30 15:12:52 -0600

    changed to use props

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,11 +4,13 @@ import { Card, Button } from "react-bootstrap"
 const Post = props => (
   <div className="p-3">
     <Card>
-      <Card.Img variant="top" src="fluffy-puppies.jpg" />
+      <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>{props.excerpt}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary" href={props.readMore}>
+          Read More...
+        </Button>
       </Card.Body>
     </Card>
   </div>
