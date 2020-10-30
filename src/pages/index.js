59647b93b38c2e39202b99be12ec7e5530e9b7e2
Talon Durrant <talonmdurrant@gmail.com>
2020-10-30 13:36:22 -0600

    homepage building out

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,24 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Post from "../components/Post"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div>
-        <h1>First Gatsby Project</h1>
-        <Link to="/test/">Navigate to Test Page</Link>
+      <div className="container">
+        <div className="row justify-content-md-center">
+          <div className="col-xs-6">
+            <Post
+              title="This is the first post"
+              excerpt="This is a fake excpert writing thingy magig for now"
+            />
+          </div>
+        </div>
       </div>
-      <img
-        src="https://i.guim.co.uk/img/media/cc5ff87a032ce6e4144e63a2a1cbe476dbc7cd5a/273_0_3253_1952/master/3253.jpg?width=700&quality=85&auto=format&fit=max&s=71c0169630a7fb2c2cf37b18affc2ead"
-        width="780"
-        alt="gatsby"
-      />
+      <Footer />
     </div>
   )
 }
