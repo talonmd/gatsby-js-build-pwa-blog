c472f3209f5fc40fc9f9c37edaf5805ed81da517
Talon Durrant <talonmdurrant@gmail.com>
2020-10-30 13:36:51 -0600

    adding Header and Footer to test page

--- src/pages/test.js
+++ src/pages/test.js
@@ -2,13 +2,15 @@ import React from "react"
 import { navigate } from "gatsby"
 
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 const Test = () => (
   <div>
-    <Header title="Header of Test Page" />
+    <Header />
     <h1>This is a test page</h1>
     <h2>This is a subheading</h2>
     <button onClick={() => navigate("/")}>Navigate to Home Page</button>
+    <Footer />
   </div>
 )
 
