8dc687c5c76217753aa0aba38bf342bb422c9d95
Talon Durrant <talonmdurrant@gmail.com>
2020-10-30 15:13:54 -0600

    change posts into pages and assign slug

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Gatsby-Bootstrap",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`, 
+    }
+  ],
 }
